1cdece1c8bc9e0d33a6e14159fff57458593789b
sammy <sammy.dev61@gmail.com>
2025-06-22 19:41:26 +0900

    [모바일청첩장] InfoSection 문구 정리

--- src/new/components/InfoSection.js
+++ src/new/components/InfoSection.js
@@ -6,9 +6,11 @@ function InfoSection() {
       <div className={styles.content}>
         <h2 className={styles.title}>We are getting married!</h2>
         <div className={styles.infoMessage}>
-          여름빛이 가득한 7월,<br/>
-          작고 소박한 예식으로 함께 걸어갈 첫걸음을 시작하려 합니다.<br/>
-          따뜻한 축복으로 소중한 순간을 함께해 주시면 감사하겠습니다.
+          여름빛이 가득한 7월, 작고 소박한 예식으로<br/>
+          함께 걸어갈 첫걸음을 시작하려 합니다.<br/>
+          <br/>
+          따뜻한 축복으로 소중한 순간을<br/>
+          함께해 주시면 감사하겠습니다.
         </div>
         <div className={styles.infoText}>
           <b>김창섭 · 윤은자</b> 의 아들 <b>김재원</b>
@@ -16,7 +18,8 @@ function InfoSection() {
           <b>故신휘철 · 박선화</b> 의 딸 <b>신새미</b>
         </div>
         <div className={styles.infoText}>
-          <b>2025년 7월 5일 토요일 오전 10시 30분</b>
+          <b>2025년 7월 5일 토요일</b><br/>
+          <b>오전 10시 30분</b>
           <div>종로 까델루뽀</div>
           {(() => {
             const weddingDate = new Date('2025-07-05');
@@ -33,7 +36,7 @@ function InfoSection() {
               dDayText = `D+${Math.abs(diffDays)}`;
             }
             
-            return <div>{dDayText}</div>;
+            return <div className={styles.dDayText}>{dDayText}</div>;
           })()}
         </div>
       </div>
