473b2898588d180109bee7d8a81ee83d2cc4e943
sammy <sammy.dev61@gmail.com>
2025-06-22 18:54:10 +0900

    [모바일청첩장] InfoSection 문구 수정

--- src/new/components/InfoSection.js
+++ src/new/components/InfoSection.js
@@ -6,11 +6,9 @@ function InfoSection() {
       <div className={styles.content}>
         <h2 className={styles.title}>We are getting married!</h2>
         <div className={styles.infoMessage}>
-          여름빛이 가득한 7월, 저희 결혼합니다.<br/>
-          고마운 분들을 모시고 예를 갖추는 것이 도리이나<br/>
-          두 사람의 뜻에 따라 소박한 결혼식을 올리게 되었습니다.<br/>
-          너그러이 헤아려 주시고 마음으로 축하해 주신다면,<br/>
-          행복하게 잘 살아가는 모습으로 보답하겠습니다.
+          여름빛이 가득한 7월,<br/>
+          작고 소박한 예식으로 함께 걸어갈 첫걸음을 시작하려 합니다.<br/>
+          따뜻한 축복으로 소중한 순간을 함께해 주시면 감사하겠습니다.
         </div>
         <div className={styles.infoText}>
           <b>김창섭 · 윤은자</b> 의 아들 <b>김재원</b>
@@ -18,7 +16,8 @@ function InfoSection() {
           <b>故신휘철 · 박선화</b> 의 딸 <b>신새미</b>
         </div>
         <div className={styles.infoText}>
-          <b>2025년 7월 5일</b>
+          <b>2025년 7월 5일 토요일 오전 10시 30분</b>
+          <div>종로 까델루뽀</div>
           {(() => {
             const weddingDate = new Date('2025-07-05');
             const today = new Date();
